7a56cea93a33beab3843a11999d5f5d47f915997
Kelthon <kelthonbalbino@gmail.com>
2023-04-28 22:39:24 -0300

    added standard_deviation.c

---
+++ src/standard_deviation.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <omp.h>
+#include <math.h>
+
+#include "benchmark.c"
+#include "average_vector_sample.c"
+
+#define CP_NUMBER_THREADS 4
+
+float cp_standard_deviation(float *vector)
+{
+    
+    float standard_deviation, average = cp_serial_average(vector), sum = 0;
+    #pragma omp parallel num_threads(CP_NUMBER_THREADS)
+    {
+
+        #pragma omp for firstprivate(sum)
+        for(int i = 0; i < CP_VECTOR_MAX_SIZE; i++)
+        {
+            sum += powf(vector[i] + average, 2);
+        }
+
+    standard_deviation = sqrtf(sum / CP_VECTOR_MAX_SIZE);
+    }
+}
+
+float serial_standard_deviation(float *vector)
+{
+    float standard_deviation, average = cp_serial_average(vector), sum = 0;
+
+    for(int i = 0; i < CP_VECTOR_MAX_SIZE; i++)
+    {
+        sum += powf(vector[i] + average, 2);
+    }
+
+    standard_deviation = sqrtf(sum / CP_VECTOR_MAX_SIZE);
+}
+
+int main()
+{
+    printf("%d\n", cp_standard_deviation(cp_random_vector(0, 100)));
+
+    return 0;
+}
